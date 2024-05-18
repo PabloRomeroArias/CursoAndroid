9b9e761f930f4b7803fcc40c26611a17583ea15e
Pablo Romero Arias <pablo@Pablos-MacBook-Pro.local>
2024-05-18 11:03:25 +0200

    Initial commit

---
+++ app/src/main/java/com/devepoler/cursoandroid/kotlin/HiWorld.kt
@@ -0,0 +1,5 @@
+package com.devepoler.cursoandroid.kotlin
+// fun == function main == kotlin entry point
+fun main(){ // la apertura y cierre de llaves indican un bloque de código
+    print("Esto mola un montón")
+}
