c0c3a2900d7752f27e0a8edff2793531ba098b36
Pablo Romero <pablo.romeroarias.main@gmail.com>
2024-05-18 11:08:31 +0200

    Gitignore updated

--- app/src/main/java/com/devepoler/cursoandroid/kotlin/HiWorld.kt
+++ app/src/main/java/com/devepoler/cursoandroid/kotlin/HiWorld.kt
@@ -2,5 +2,4 @@ package com.devepoler.cursoandroid.kotlin
 // fun == function main == kotlin entry point
 fun main(){ // la apertura y cierre de llaves indican un bloque de código
     print("Esto mola un montón")
-    print("Esto mola un montón")
 }
