23d477cb7d6bdb575bdfdcd82b337f94f3af0184
Pablo Romero <pablo.romeroarias.main@gmail.com>
2024-11-13 18:46:07 +0100

    Login and home implemented

---
+++ app/src/main/java/com/devepoler/cursoandroid/android/data/bo/LoginFormBo.kt
@@ -0,0 +1,7 @@
+package com.devepoler.cursoandroid.android.data.bo
+
+// BO -> Business Object
+data class LoginFormBo(
+    val email : String?,
+    val password : String?
+)
---
+++ app/src/main/java/com/devepoler/cursoandroid/android/ui/login/LoginFragment.kt
@@ -0,0 +1,56 @@
+package com.devepoler.cursoandroid.android.ui.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.devepoler.cursoandroid.android.data.LoginResponse
+import com.devepoler.cursoandroid.android.data.bo.LoginFormBo
+import com.devepoler.cursoandroid.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private val binding by lazy { FragmentLoginBinding.inflate(layoutInflater) }
+    private val viewmodel by viewModels<LoginViewModel>()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = binding.root
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        with(binding) {
+            btLogin.setOnClickListener {
+                when (val response = viewmodel.login(getLoginFormBo())) {
+                    is LoginResponse.Error -> Toast.makeText(
+                        root.context,
+                        response.msg,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    is LoginResponse.Success -> navToHomeFragment(response.user.name)
+                }
+            }
+
+            btGuess.setOnClickListener { navToHomeFragment() }
+
+        }
+    }
+
+    private fun navToHomeFragment(name : String? = null) {
+        val direction = LoginFragmentDirections.actionFirstFragmentToSecondFragment(name)
+        findNavController().navigate(direction)
+    }
+
+    private fun getLoginFormBo() : LoginFormBo {
+        val email = binding.tilEmail.editText?.text.toString()
+        val password = binding.tilPassword.editText?.text.toString()
+        return LoginFormBo(email, password)
+    }
+}
